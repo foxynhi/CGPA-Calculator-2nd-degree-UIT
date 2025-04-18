b00dc5534986c2c4cb868639ad6b559547cdfa1f
nhi <nhi-mac@MacBookAir.lan>
2025-04-18 10:45:16 +0700

    commit changes

--- CourseList.h
+++ CourseList.h
@@ -19,6 +19,7 @@ struct CourseList
 
     //Initialize Course objects from CSV
     void initDataFromCSV(const string& file);
+    Node* findNode(string targetId, int sem);
     bool deleteCourse(string targetId, int sem);
     void updateCourse(string targetId, string targetName, int tolCre, int lecCre, int labCre, int p, int sem);
 };
